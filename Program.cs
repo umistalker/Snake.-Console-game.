e8b3ea8f2cf203aaca8d31f4b6b0ada5162fab37
Mark <umistalker@gmail.com>
2021-04-17 11:42:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+    }
+}
